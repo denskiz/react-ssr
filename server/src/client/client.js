b44d77206e32dde23f1b5770342c4993cb36fa40
Dennis Ent <dennisent@Denniss-MacBook-Pro.local>
2018-10-19 19:57:32 +1100

    friday code

--- server/src/client/client.js
+++ server/src/client/client.js
@@ -1,20 +1,21 @@
 // Startup point for the client side application
-
+// import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Routes from './Routes';
-import { createStore, applyMiddleware } from 'redux'
-import thunk from 'redux-thunk'
-import { Provider} from 'react-redux'
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import reducers from './reducers';
 
-const store = createStore(reducers, {}, applyMiddleware(thunk))
+const store = createStore(reducers, {}, applyMiddleware(thunk));
 
 // Re hydrate application on the browser
 ReactDOM.hydrate(
   <Provider store={store}>
-   <BrowserRouter>
-     <Routes />
+    <BrowserRouter>
+      <Routes />
     </BrowserRouter>
   </Provider>,
   document.querySelector('#root')
